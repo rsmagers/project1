7b27b1028acd9e391e4f4645716bfc1b92d80265
Ryan <ryan.s.magers@gmail.com>
2020-04-02 17:43:05 +0000

    table done

--- project1.c
+++ project1.c
@@ -10,6 +10,10 @@ double turnaround = 0.00;
 double waiting;
 double response;
 FILE *fp;
+int row = 0;
+int P; 
+int max;
+int total = 0;
 
 void printAll()
 {
@@ -35,12 +39,91 @@ void next3(int* temp)
 	}
 }
 
+void poptable(int ID, int burst, int table[max][P], int respt[])
+{
+	int stopPoint = row + burst;
+	if(respt[ID] == -1 || respt[P-1] == 1)
+	{
+		respt[ID] = row;
+		//printf("respt[%d] = %d\n",ID,respt[ID]);
+	}
+	for(; row < stopPoint; row++)
+	{
+		//printf("poptable[%d][%d]", row, ID);
+		table[row][ID] = 1;
+		//printf(" = %d\n", table[row][ID]);	
+	}
+	//printf("\n");
+}
+
+void fixTable(int table[max][P])
+{
+	int first1 = 0;
+	int last1 = 0;
+	int one = 0;
+	int count = 0;
+	
+	for(int i = 0; i < P; i++) // col
+	{
+		count = 0;
+		first1 = 0;
+		last1 = 0;
+		one = 0;
+
+		for(int x = 0; x < total; x++) // row
+		{
+			if(table[x][i] == 1) // this sets the flag for the first ID
+			{
+				one = 1;
+			}
+			// this if fills in the first1 if it didnt start first
+			if(table[x][i] == 0 && one == 0)
+			{
+				table[x][i] = 1;
+				one = 1;
+				first1 = x; // should always be 0;
+				//printf("ID = %d, first1 = %d\n", i, first1);
+			}
+			// this if finds last 1
+			if(table[x][i] == 1 && one == 1)  // error here
+			{				
+				last1 = x;
+				//printf("ID = %d, last1 = %d\n", i, last1);
+			}
+			else
+			{
+				count++; // only kept for troubleshooting
+			}
+			
+		}
+		
+		for(int z = first1; z < last1 - 1; z++)
+		{
+			table[z][i] = 1; // error here
+			//printf("fixing table[%d][%d]\n", z, i);
+		}
+		
+		
+	}
+}
+
+void printtable(int table[max][P])
+{
+	for(int i = 0; i < total; i++)
+	{
+		for(int y = 0; y < P; y++)
+		{
+			printf(" %d ", table[i][y]);
+		}
+		printf("\n");
+	}
+}
+
 int main(int argc, char** argv)
 {
 	// setup
 	fp = fopen(argv[1], "r");
 	
-	int P;
 	fscanf(fp, "%d", &P); // numb processes. in this project always = 1
 	fscanf(fp, "%d", &P); // numb of execution elements
 	int lines;
@@ -49,89 +132,80 @@ int main(int argc, char** argv)
 	int temp[3];
 	int master[P];
 	int switchArray[P];
+	int respT[P];	
 	for(int a = 0; a < P; a++) // init both
 	{
 		master[a] = 0; 
 		switchArray[a] = 0;
+		respT[a] = -1;
 	}
+	max = P * 1024;
+	int myTable[max][P];
+	for(int b = 0; b < max; b++)
+	{
+		for(int pop = 0; pop < P; pop++)
+		{
+			myTable[b][pop] = 0;
+		}
+	}
+
 
 	// build
 	int pre = 0;
 	int cur;	
-	int total = 0;
+	
 	for(int i = 0; i < lines; i++) 
 	{
-		next3(temp);
-		master[temp[0]-1] += temp[1];
-		cur = temp[0];
+		next3(temp); // gets numbers
+		
+		poptable(temp[0]-1, temp[1],myTable, respT); // pops table
 
-		if(pre == 0) // this sets up pre, prob delete
-		{
-			//pre = cur;
-		}
+		master[temp[0]-1] += temp[1];
 
 		total += temp[1];
-		//turnaround += temp[1];
-
-		if(cur != pre) // a switch happens
+		// now counting switches
+		cur = temp[0];
+		if(cur != pre)
 		{
 			if(switchArray[temp[0]-1] == 0) // if first time increase  vcon
 			{
 				switchArray[pre - 1] = 1;
-				//printf("switchArray change = %d\n", switchArray[pre - 1]);
-				vcon++; 
-				//response += total;
-				//printf("response = %f\n", response);
+				vcon++; 	
 			}
 			else if(switchArray[temp[0]-1] == 1)
 			{
-				//printf("invcon switch = %d\n", switchArray[pre - 1]);
 				invcon++;
-				
 			}
-
 		}
-		
-		/*  Old switch counter
-		// count switches	
-		if(pre < temp[0])
-		{
-			pre = temp[0];
-			vcon++;
-		}
-		else if(pre > temp[0])
-		{
-			pre = temp[0];
-			invcon++;
-			turnaround += temp[1];
-		}
-		//printf("total = %d\n", total);
-		//printf("turnaround = %f\n", turnaround);
-		*/
-		//printf("pre = %d, cur = %d\n", pre, cur);
-		
 		pre = cur;
 	}
-	for(int x = 0; x < P; x++)
-	{
-		turnaround += master[x] * (P - x);
-	}
-
-
+	
+	fixTable(myTable);	
+	//printtable(myTable);
+	int sum = 0;
 	//stats
-	//printf("total = %d\n", total);
-	//printf("turnaround = %f\n", turnaround);
-	//printf("response = %f\n", response);	
+	for(int t = 0; t < total; t++)
+	{
+		for(int r = 0; r < P; r++)
+		{
+			if(myTable[t][r] == 1)
+			{
+				sum++;
+			}
+		}
+	}
+	
 	// calc
-	waiting = (double) (turnaround - total) / P; 
-
-	response = (double) (turnaround - total) / P; // this is wrong
-
-	turnaround = (double) turnaround  / P;		
-	
+	turnaround = (double) sum / P;
+	waiting = (double) (sum - total) / P;
 	thrput = (double) P / total;
-	
-	
+	sum = 0;
+	for(int g = 0; g < P; g++)
+	{
+		sum += respT[g];
+		//printf("sum = %d, why = %d\n",sum, respT[g]);
+	}
+	response = (double) sum / P;
 	// print
 	printAll();
 
